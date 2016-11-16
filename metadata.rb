bead9e94d460f0fe23069818348fad761cb1c14b
robkirkcom <rob@robkirk.com>
2016-11-16 12:23:50 -0600

    second commit

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+name             "apache2"
+description      "Installs and configures all aspects of apache2 using Debian style symlinks with helper definitions"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+maintainer       "AWS OpsWorks"
+license          "Apache 2.0"
+version          "2.0.0"
