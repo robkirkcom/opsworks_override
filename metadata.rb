936fc75878e70024ad2a1f9451fcafe7a9c0214f
robkirkcom <rob@robkirk.com>
2016-11-16 12:31:08 -0600

    third commit

--- metadata.rb
+++ metadata.rb
@@ -3,4 +3,4 @@ description      "Installs and configures all aspects of apache2 using Debian st
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 maintainer       "AWS OpsWorks"
 license          "Apache 2.0"
-version          "2.0.0"
+version          "0.1.0"
